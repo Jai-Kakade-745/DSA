f42f2a32dfd35480f1ba95fb10c799db8d39325d
jai kakade <theconquererj@gmail.com>
2026-05-24 12:26:36 +0530

    Create input.java

---
+++ Basics/input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class main{
+  public static void main(String args[]}{
+    Scanner sc = new Scanner(System.in);
+    int age = sc.nextInt();
+  sc.close();
+  }
+}
+
+// input in Java
+// byte a = sc.nextByte();
+// short a = sc.nextShort();
+// int a = sc.nextInt();
+// long a = sc.nextLong();
+// float a = sc.nextFloat();
+// double a = sc.nextDouble();
+// boolean a = sc.nextBoolean();
+// String s = sc.next(); 
+// String s = sc.nextLine(); 
+// char c      = sc.next().charAt(0);
