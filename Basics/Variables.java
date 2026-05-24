82b98cdbb33a88d135622fd650ffcaa53a4a4dbb
jai kakade <theconquererj@gmail.com>
2026-05-24 12:14:08 +0530

    Document variable types and naming conventions
    Added comments explaining variable types and conventions in Java.

--- Basics/Variables.java
+++ Basics/Variables.java
@@ -1,1 +1,29 @@
+// variables - A variable is a named container that stores data in memory
+int age = 20;
 
+//Types of variables
+// Local Variable -> Declared inside a method, Only accessible within that method, Must be initialized before use
+void show() {
+    int x = 10;  // local variable
+    System.out.println(x);
+}
+
+// Instance Variable -> Declared inside a class, outside methods,Each object gets its own copy, Gets a default value automatically
+class Student {
+    String name;  // instance variable
+    int age;      // instance variable
+}
+
+// Static Variable -> Declared with static keyword,Shared among all objects of the class,Only one copy exists in memory
+class Student {
+    static String school = "Gujarat University";  // static variable
+}
+
+// variable name can start with _ , $ 
+// cannot start with number , case sensitive and keywords are not allowed
+// convention - Always use camelCase
+
+// In java always use final keyword instead of const keyword
+final int MAX_MARKS = 100;
+final double PI = 3.14159;
+final String COLLEGE = "Gujarat University";
