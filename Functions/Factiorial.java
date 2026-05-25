1f7fdd1b8cfb602c42ae0f7f55ef7770daec7be3
jai kakade <theconquererj@gmail.com>
2026-05-26 00:45:13 +0530

    Update Factiorial.java

--- Functions/Factiorial.java
+++ Functions/Factiorial.java
@@ -18,3 +18,31 @@ public class hello{
         
     }
 }
+
+// calculate the binomial coefficient
+import java.util.*;
+public class hello{
+    public static int factorial(int n){
+        int f = 1;
+        for(int i = 1; i <= n ; i++){
+            f *= i;
+            }
+        return f;
+    }
+
+    public static int binomial(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr= factorial(n-r);
+        int binomial_coefficient= fact_n/(fact_r * fact_nmr);
+        return binomial_coefficient;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int result = binomial(n,r);
+        System.out.println("the binomial coefficient is : "+result);
+        
+    }
+}
