fa25a51747b1cd939d045a7a6be25531cee236b1
jai kakade <theconquererj@gmail.com>
2026-05-29 13:13:31 +0530

    Add binary search algorithm comments
    Add comments explaining the binary search algorithm.

---
+++ Arrays/Binary_Search.java
@@ -0,0 +1,10 @@
+//Binary search
+//prerequisite
+
+// Instead of checking every element (linear search), binary search always checks the middle element and eliminates 
+// half the remaining elements each step.
+
+// first it takes a middle element , 
+//If the( Key > Middle element) then search for second half
+// If (Key < Middle element) then search for first half 
+// if ( Key = Middle element ) then return middle element
